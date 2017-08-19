3e2389e4856e5077db11a1d5329f14556962adf6
kZime <liuyimu2015@hotmail.com>
2017-08-19 12:08:17 +0800

    删除二进制文件, 更改安装方式, 还原oalj_color.cpp

--- install.py
+++ install.py
@@ -6,7 +6,7 @@ import os
 fst = input("你想安装OALJ(OIer && ACMer's Local Judge)吗? (y/n)")
 if fst == 'y':
     os.system('cp oalj.py /usr/bin/oalj')
-    os.system('cp oalj_color /usr/bin/oalj_color')
+    os.system('g++ oalj_color.cpp -o /usr/bin/oalj_color -g')
 else:
     sec = input("你想卸载OALJ(OIer && ACMer's Local Judge)吗? (y/n)")
     if sec == 'y':
