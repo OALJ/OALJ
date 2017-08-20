032a44b65bb928c81e41ac4aa1476bead34aaabc
Margatroid <351652632@qq.com>
2017-08-20 10:46:36 -0400

    更新 'install.py'
    改掉一个语法错误

--- install.py
+++ install.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# !/usr/bin/env python3
+# coding = utf-8
 import sys
 import os
-
-fst = input("你想安装OALJ(OIer && ACMer's Local Judge)吗? (y/n)")
-if fst == 'y':
+if input("你想安装OALJ(OIer && ACMer's Local Judge)吗? (y/n)") == 'y':
     os.system('cp oalj.py /usr/bin/oalj')
     os.system('cp oalj_color /usr/bin/oalj_color')
-    pip install colorama
-
-else:
-    sec = input("你想卸载OALJ(OIer && ACMer's Local Judge)吗? (y/n)")
-    if sec == 'y':
-        os.system("rm /use/bin/oalj")
-        os.system('rm /usr/bin/oalj_color')
+    os.system("pip install colorama")
+elif input("你想卸载OALJ(OIer && ACMer's Local Judge)吗? (y/n)"): == 'y'
+    os.system("rm /use/bin/oalj")
+    os.system('rm /usr/bin/oalj_color')
+finally: print("setup finished")
