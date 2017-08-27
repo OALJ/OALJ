aa3d7abfff02341c13f15ba3d1a57580fc0ab615
Margatroid <351652632@qq.com>
2017-08-27 10:07:17 +0800

    fix install.py

--- install.py
+++ install.py
@@ -1,13 +1,16 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 import sys
 import os
-if input("你想安装OALJ(OIer && ACMer's Local Judge)吗? (y/n)") == 'y':
+
+fst = input("你想安装OALJ(OIer && ACMer's Local Judge)吗? (y/n)")
+if fst == 'y':
     os.system('cp oalj.py /usr/bin/oalj')
     os.system('cp oalj_color /usr/bin/oalj_color')
     os.system("pip install colorama")
-    os.system("pip install psutil")
-elif input("你想卸载OALJ(OIer && ACMer's Local Judge)吗? (y/n)"): == 'y'
-    os.system("rm /use/bin/oalj")
-    os.system('rm /usr/bin/oalj_color')
-finally: print("setup finished")
+    os.system("pip install request")
+else:
+    sec = input("你想卸载OALJ(OIer && ACMer's Local Judge)吗? (y/n)")
+    if sec == 'y':
+        os.system("rm /use/bin/oalj")
+        os.system('rm /usr/bin/oalj_color')
