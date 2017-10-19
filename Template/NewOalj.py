d469ad2acced388e21e18610d68899af08e13ca7
kzime <liuyimu2015@hotmail.com>
2017-10-19 13:04:01 +0800

    运行

--- Template/NewOalj.py
+++ Template/NewOalj.py
@@ -5,12 +5,14 @@ import os
 import json
 import sys
 import time
+import psutil
+from subprocess import Popen
 from colorama import init, Fore
 
 # 编译选项
 COMPILEARGV = "-DLOCAL"
 # diff选项
-DIFFARGV = "-U 0 -b -B -w"
+DIFFARGVS = "-U 0 -b -B -w"
 # 运行模式
 Mode = "Normal"
 ColorMap = {
@@ -50,7 +52,7 @@ def checkMode():
     global COMPILEARGV
     argvs = sys.argv
     if "-r" in argvs:
-        all_clean()
+        allClean()
         if os.path.isdir("data"):
             os.rmdir("data &> /dev/null")
         if os.path.isfile("config.json"):
@@ -61,7 +63,7 @@ def checkMode():
         mode = "Lunatic"
         argvs.remove("-q")
     if "-i" in argvs:
-        Problem["Name"] = argvs[argvs.index("-i") + 1]
+        Problem["File"] = argvs[argvs.index("-i") + 1]
         argvs.remove(argvs[argvs.index("-i") + 1])
         argvs.remove("-i")
     for CppLevel in argvs:
@@ -98,8 +100,9 @@ def compileProgram(file):
 '''
 
 {
-    "Name": "aplusb.cpp",
-    "List": [1, 10],
+    "File": "aplusb.cpp"
+    "Name": "aplusb",
+    "List": [1],
     "Input": ".in",
     "Output": ".ans",
     "MemoryLimit": 256,
@@ -115,28 +118,86 @@ def isdigit(x):
         return 0
     return 1
 
+
+def get_process_memory(p):
+    try:
+        MemInfo = str(p.memory_info())
+    except Exception:
+        return 0
+    RssBytes = int(MemInfo.split(',')[0].split('=')[-1])
+    VmsBytes = int(MemInfo.split(',')[1].split('=')[-1])
+    MemAll = float(RssBytes + VmsBytes) / 1024 / 1024
+    return MemAll
+
+
+
 def judge():
     global Problem
     global Mode
-    FileName = Problem["Name"]
+    DataName = Problem["Name"]
     Queue = Problem["List"]
     if len(Queue) == 2 and isdigit(Queue[0]) and isdigit(Queue[1]):
         Queue = list(range(Queue[0], Queue[1] + 1))
     Queue = list(map(str, Queue))
     Input = Problem["Input"]
-    Output = Problem["Input"]
-    MemoryLimit = Problem["MemoryLimit"]
-    TimeLimit = Problem["TimeLimit"]
-    UnitScore = round(100 / len)
+    Output = Problem["Output"]
+    MemoryLimit = Problem["MemoryLimit"]  # MB
+    TimeLimit = Problem["TimeLimit"] # MS
+    UnitScore = round(100 / len(Queue), 1)
     Score = 0
     Num = 0
+    TimeAll = 0
     Ans = True
     if Mode == "Normal":
-        color_print("序号\t结果\t时间\t内存\t返回值\t得分", 7)
-    
-    for tmp in Queue:
+        colorPrint("序号\t结果\t时间\t内存\t返回值\t得分\n", 7)
+    else:
+        Result = ""
+    for now in Queue: # str in Queue
         Num += 1
-        
+        InputFileName = DataName + now + Input
+        OutputFileName = DataName + now + Output
+        ProcessStatus = "Running"
+        BeginTime = time.time()
+        InputFile = open("data/%s" % InputFileName, "w")
+        OutputFile = open("/tmp/oalj/Output", "w")
+        ErrFile = open("/tmp/oalj/Running.log", "w")
+        ChildProcess = Popen("/tmp/oalj/main", 0, None, stdin = InputFile, stdout = OutputFile, stderr = ErrFile)
+        MaxMemoryUsed = 0
+        p = psutil.Process(ChildProcess.pid)
+        time.sleep(0.002)
+        while ChildProcess.poll() == None:
+            mem = get_process_memory(p)
+            if time.time() - BeginTime > float(TimeLimit):
+                ChildProcess.kill()
+                ProcessStatus = "TLE"
+            if mem * 1024 > MemoryLimit:
+                ChildProcess.kill()
+                ProcessStatus = "MLE"
+            MaxMemoryUsed = max(MaxMemoryUsed, mem)
+        TimeUsed = float(time.time() - BeginTime) * 1000
+        ChildProcess.poll()
+        ReturnCode = ChildProcess.returncode
+        InputFile.close()
+        OutputFile.close()
+        ErrFile.close()
+        MemoryUsed = MaxMemoryUsed
+        TimeAll += TimeUsed
+        DiffReturn = os.system("diff %s /tmp/oalj/Output data/%s >> /tmp/oalj/Diff.log" % (DIFFARGVS, OutputFileName))
+
+        if ProcessStatus == "MLE":
+            if Mode == "Lunatic":
+                Result += "M"
+                continue
+            colorPrint(" %s\t" %str(Num), 7)
+            colorPrint("MLE\t", 3)
+            colorPrint("{0:.0f}ms\t{1:.2f}MB\t{2}\t{3:.0f}\n".format(TimeUsed, MemoryUsed, ReturnCode, 0), 7)
+            if Ans:
+                Ans = False
+                os.system("cp data/%s /tmp/oalj/FalseInput" % InputFileName)
+                os.system("cp /tmp/oalj/Output /tmp/oalj/FalseOutput")
+                os.system("cp data/%s /tmp/oalj/FalseAns" % OutputFileName)
+
+
 
 
 if __name__ == '__main__':
@@ -144,11 +205,11 @@ if __name__ == '__main__':
     Problem = config()
     checkMode()
     os.mkdir("/tmp/oalj")
-    if compileProgram(Problem["Name"]):
+    if compileProgram(Problem["File"]):
         if os.path.isfile("/tmp/oalj/Compile.log"):
             with open("/tmp/oalj/Compile.log", "r") as cl:
                 for line in cl:
-                    color_print(line + "\n", 1)
+                    colorPrint(line + "\n", 1)
         else:
             colorPrint("没有评测代码， 请检查文件目录或者config.json\n", 1)
         linePrint()
--- Template/aplusb.cpp
+++
@@ -1,7 +0,0 @@
-# include <bits/stdc++.h>
-using namespace std;
-int main() {
-    double  a, b;
-    cin >> a >> b;
-    cout << (int) (a + b);
-}
